5164bfbd0e3f241b04814657abc54fab4c66d1a4
Pit Garbe <leberwurstsaft@me.com>
2013-06-29 21:05:09 +0200

    Prepare Opensourcing

---
+++ Classes/TestScene.h
@@ -0,0 +1,16 @@
+//
+//  TestScene.h
+//  Hemispheres2
+//
+//  Created by Pit Garbe on 05.03.11.
+//  Copyright 2011 Pit Garbe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TestScene : NSObject {
+    
+}
+
+@end
---
+++ Classes/TestScene.m
@@ -0,0 +1,14 @@
+//
+//  TestScene.m
+//  Hemispheres2
+//
+//  Created by Pit Garbe on 05.03.11.
+//  Copyright 2011 Pit Garbe. All rights reserved.
+//
+
+#import "TestScene.h"
+
+
+@implementation TestScene
+
+@end
